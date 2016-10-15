46a11f97ca275b3d2b53381003f34a0e89c2aa81
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-15 11:31:57 -0400

    creation

---
+++ to_array.py
@@ -0,0 +1,20 @@
+def to_array(a, frmt='{:1.2f}', arraytype='bmatrix'):
+    """Returns a LaTeX array
+    :a: numpy array
+    :returns: 
+    :prints: LaTeX array
+
+    Example:
+    to_array(Krm, frmt = '{:6.2f}', arraytype = 'array')
+    """
+    if len(a.shape) > 2:
+        raise ValueError('bmatrix can at most display two dimensions')
+    lines = str(a).replace('[', '').replace(']', '').splitlines()
+    print(r'\begin{' + arraytype + '}')
+    for l in lines:
+        for num in l.split():
+            print(frmt.format(float(num)) + ' ', end="")
+            print('& ', end="")
+        print(r'\\')
+    print(r'\end{' + arraytype + '}')
+    return

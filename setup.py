5f0228f41918f7588b9b35193d0f7c521274cd82
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-15 12:42:04 -0400

    first

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='array_to_latex',
+      version='0.1',
+      description='Numpy array formatted as LaTeX',
+      author='Joseph C. SLater',
+      author_email='joseph.c.slater@gmail.com',
+      url='https://pypi.python.org/pypi/array_to_latex',
+      packages=['array_to_latex'],
+      )

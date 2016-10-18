d8b542c6db1fbfc6a2809168ba6fd7d7d4dcc2db
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-17 22:30:14 -0400

    "last" push to github to start delivery.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='array_to_latex',
-      version='0.1',
-      description='Numpy array formatted as LaTeX',
-      author='Joseph C. SLater',
+      version='0.1b',
+      description='Numpy array returned as formatted LaTeX array',
+      author='Joseph C. Slater',
       author_email='joseph.c.slater@gmail.com',
-      url='https://pypi.python.org/pypi/array_to_latex',
+      url='https://github.com/josephcslater/array_to_latex/'
+      download_url='https://github.com/josephcslater/array_to_latex/archive/0.1b.tar.gz',
       packages=['array_to_latex'],
+      keywords=['latex','array','format']
       )

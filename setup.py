b2e1c58ebe120e899bc38bb393734c4188339818
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-19 23:58:41 -0400

    new version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='array_to_latex',
-      version='0.1b',
+      version='0.201b',
       description='Numpy array returned as formatted LaTeX array',
       author='Joseph C. Slater',
       author_email='joseph.c.slater@gmail.com',
       url='https://github.com/josephcslater/array_to_latex/',
-      download_url='https://github.com/josephcslater/array_to_latex/archive/0.1b.tar.gz',
+      download_url='https://github.com/josephcslater/array_to_latex/archive/0.11b.tar.gz',
       packages=['array_to_latex'],
       keywords=['latex','array','format']
       )

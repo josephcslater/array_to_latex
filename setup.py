c6d120a48b5f6fbfce348121451f56d8595c9f2f
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-29 11:07:48 -0400

    Let's provide better docs for the web.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from distutils.core import setup
 
 setup(name='array_to_latex',
       version='0.30b',
-      description='Numpy arrays converted to formatted LaTeX array',
+      description='Return Numpy arrays as formatted LaTeX arrays',
       author='Joseph C. Slater',
       author_email='joseph.c.slater@gmail.com',
       url='https://github.com/josephcslater/array_to_latex/',
       download_url='https://github.com/josephcslater/array_to_latex/archive/0.30b.tar.gz',
       packages=['array_to_latex'],
+      long_description = read('README.rst')
       keywords=['latex','array','format']
       )

469ab277127c783b43038863e11be00768b58dce
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-31 20:59:57 -0400

    updated license

--- setup.py
+++ setup.py
@@ -12,17 +12,34 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='array_to_latex',
-      version='0.35',
+      version='0.36',
       description='Return Numpy arrays as formatted LaTeX arrays.',
       author='Joseph C. Slater',
       author_email='joseph.c.slater@gmail.com',
       url='https://github.com/josephcslater/array_to_latex/',
-      download_url='https://github.com/josephcslater/array_to_latex/archive/0.35.tar.gz',
+      download_url='https://github.com/josephcslater/array_to_latex/archive/0.36.tar.gz',
       packages=['array_to_latex'],
       long_description = read('README.rst'),
-      keywords=['latex','array','format','numpy'],
-      install_requires=[
-      'numpy',
-      'clipboard'
-      ]
+      keywords=['latex','array','format','numpy','scipy'],
+      install_requires=['numpy','clipboard'],
+      classifiers = ['Development Status :: 5 - Production/Stable',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: MIT License',
+                    'Environment :: Console',
+                    'Intended Audience :: End Users/Desktop',
+                    'Intended Audience :: Education',
+                    'Intended Audience :: Science/Research',
+                    'Programming Language :: Python',
+                    'Programming Language :: Python :: 3.4',
+                    'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
+                    'Topic :: Scientific/Engineering',
+                    'Topic :: Text Processing :: Markup :: LaTeX',
+                    'Operating System :: Microsoft :: Windows',
+                    'Operating System :: POSIX',
+                    'Operating System :: Unix',
+                    'Operating System :: MacOS',
+                    'Topic :: Utilities']
       )
+
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
